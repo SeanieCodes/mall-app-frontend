18ade552b95ade79daec323b1b921ce21474ab28
SeanieCodes <seanhello12321@gmail.com>
2025-02-11 16:10:16 +0800

    refactor: simplify code

--- src/components/Layout/Dropdown.jsx
+++ src/components/Layout/Dropdown.jsx
@@ -1,16 +1,25 @@
 import Select from 'react-select';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Dropdown = ({ onUserTypeChange }) => {
+const Dropdown = () => {
+
+  const [selectedType, setSelectedType] = useState('shopper');
+  const navigate = useNavigate();
+  
   const options = [
     { value: 'shopper', label: 'Shopper' },
     { value: 'staff', label: 'Staff' }
   ];
+  const handleChange = (option) => {
+    setSelectedType(option.value);
+  };
 
   return (
     <Select
       options={options}
       defaultValue={options[0]}
-      onChange={(option) => onUserTypeChange(option.value)}
+      onChange={handleChange}
     />
   );
 };
