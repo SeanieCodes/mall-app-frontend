3488a273eea3b98bff10147a3299bf2d76ab0751
SeanieCodes <seanhello12321@gmail.com>
2025-02-11 17:17:16 +0800

    refactor: take dropdown import out of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dropdown from './components/Layout/Dropdown';
 
 function App() {
 
