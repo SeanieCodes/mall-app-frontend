f2ec8d1bf824348ef3d48a1cc245b8fde9f1bc33
SeanieCodes <seanhello12321@gmail.com>
2025-02-12 15:38:12 +0800

    feat: add dropdown css

--- src/components/Layout/Dropdown.jsx
+++ src/components/Layout/Dropdown.jsx
@@ -1,5 +1,6 @@
 import Select from 'react-select';
 import { useNavigate } from 'react-router-dom';
+import './Dropdown.css';
 
 const Dropdown = ({onTypeChange}) => {
 
@@ -18,6 +19,7 @@ const Dropdown = ({onTypeChange}) => {
       options={options}
       defaultValue={options[0]}
       onChange={handleChange}
+      classNamePrefix="select"
     />
   );
 };
