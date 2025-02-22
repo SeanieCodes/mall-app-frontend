3cb7d8a466e340775e79ce3f121728672da10b89
SeanieCodes <seanhello12321@gmail.com>
2025-02-22 14:28:39 +0800

    fix: fix App and main bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { UserProvider } from './contexts/UserContext.jsx';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
     <UserProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </UserProvider>
-    </BrowserRouter>
   </StrictMode>
 );
